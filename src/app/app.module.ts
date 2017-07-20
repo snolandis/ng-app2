cd148dd05410b22a4b5e4c0b5113d8c2c3831da0
Stephen Landis <stephen.n.landis@gmail.com>
2017-07-19 21:13:02 -0500

    routes finally making sense

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { FeaturesComponent } from './features/features.component';
 import { FeatureCardComponent } from './feature-card/feature-card.component';
 import { FeatureDetailsComponent } from './feature-details/feature-details.component';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'features', component: FeaturesComponent },
+  { path: 'feature-card', component: FeatureCardComponent },
+];
 
 @NgModule({
   declarations: [
@@ -16,7 +23,8 @@ import { FeatureDetailsComponent } from './feature-details/feature-details.compo
     FeatureDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
