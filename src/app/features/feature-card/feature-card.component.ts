f81e4bec7bfacc7520a5da7ab578f590bc871820
Stephen Landis <stephen.n.landis@gmail.com>
2017-07-31 14:12:14 -0500

    combined features dirs

---
+++ src/app/features/feature-card/feature-card.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-feature-card',
+  templateUrl: './feature-card.component.html',
+  styleUrls: ['./feature-card.component.css']
+})
+export class FeatureCardComponent implements OnInit {
+//   features: [
+//     {
+//       "slug": "feature-title-1",
+//       "title": "Feature Title 1",
+//       "excerpt": "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+//       "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+//       "image":"http//placehold.it/600x300"
+//     },
+//    {
+//       "slug": "feature-title-2",
+//       "title": "Feature Title 2",
+//       "excerpt": "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+//       "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+//       "image":"http//placehold.it/600x300"
+//     }
+//
+//
+//
+//
+// ]
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
