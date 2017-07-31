eb989cef67c3a75b6e36656e0529935af25c9ba9
Stephen Landis <stephen.n.landis@gmail.com>
2017-07-31 14:41:40 -0500

    fixed app-feature-card, need to create the ngFor

--- src/app/features/features.component.ts
+++ src/app/features/features.component.ts
@@ -6,26 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./features.component.css']
 })
 export class FeaturesComponent implements OnInit {
-//   features: [
-//     {
-//       "slug": "feature-title-1",
-//       "title": "Feature Title 1",
-//       "excerpt": "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
-//       "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
-//       "image":"http//placehold.it/600x300"
-//     },
-//    {
-//       "slug": "feature-title-2",
-//       "title": "Feature Title 2",
-//       "excerpt": "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
-//       "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
-//       "image":"http//placehold.it/600x300"
-//     }
-//
-//
-//
-//
-// ]
+  features: [
+    {
+      "slug": "string",
+      "title": "string",
+      "excerpt": "string",
+      "description": "string",
+      "image": "string",
+    }
+]
 
 
 
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Feature } from '../features/features.component';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  numberId = 1;
+  features: Feature[];
+
+
   constructor() { }
 
   ngOnInit() {
